265aa773629b64538152c4246e218e2d4ca9b090
gbisht1947 <128208713+gbisht1947@users.noreply.github.com>
2023-03-24 13:26:12 +0530

    Add files via upload

---
+++ jsjs.js
@@ -0,0 +1,9 @@
+burger = document.queryselector('.burger')
+nav = document.queryselector('.nav')
+burger = document.queryselector('.burger')
+
+burger.addEventListener('click',()=>{
+    nav.classlist.toggle("vclass");
+}
+
+)
